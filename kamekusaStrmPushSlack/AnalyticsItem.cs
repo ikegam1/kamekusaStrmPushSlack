29a415256ca250dd0dd0deceb2e58f04aa45038e
ikegam1 <takesiba.masrao@gmail.com>
2021-12-11 17:31:07 +0900

    first

---
+++ kamekusaStrmPushSlack/AnalyticsItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace kamekusaStreamPushSlack
+{
+    public class AnalyticsItem
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("timestamp")]
+        public ulong Timestamp { get; set; }
+
+        [JsonProperty("_ts")]
+        public uint Ts { get; set; }
+
+        [JsonProperty("bbox")]
+        public float[] BBox { get; set; }
+    }
+}
